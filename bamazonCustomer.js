c2fda8e3caf2c3bb9b6a4ae8cb67657a823a85dc
William Blake <willblakebooking@gmail.com>
2018-01-29 13:39:19 -0600

    displaying results in table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,16 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var consoleTable = require('console-table');
+var colors = require('colors');
+
+// Start set colors theme
+colors.setTheme({
+  info: 'green',
+  warn: 'yellow',
+  error: 'red'
+});
+
+// end set colors theme
 
 var con = mysql.createConnection({
   host: 'localhost',
@@ -11,14 +22,26 @@ var con = mysql.createConnection({
 
 con.connect(function (err) {
   if (err) throw err;
-  console.log('Connected!');
+  console.log('Connected!'.info);
 
-  // Display items available for sale
+  // Start display items available for sale
   con.query('SELECT item_id, product_name, price FROM products', function (err, res) {
     if (err) throw err;
-    console.log(res);
+
+    console.log('***Items for Sale***');
+
+    for (var i = 0; i < res.length; i++) {
+
+      consoleTable([
+    ['ID ', 'Product Name ', 'Price '],
+    [res[i].item_id, res[i].product_name, res[i].price],
+  ]);
+    }
+
+    console.log('------------------------');
     connection.end();
   });
+
   // End display items available for sale
 
 });
