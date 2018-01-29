7805d5085d449c6773ecbe355983c2df55ce60dc
William Blake <willblakebooking@gmail.com>
2018-01-29 11:15:33 -0600

    search available items added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,24 @@
-var mysql = require ('mysql');
-var inquirer = require ('inquirer');
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-var con = mysql.createConnection ({
+var con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  port: '8889'
+  port: '8889',
+  database: 'bamazon'
 });
 
-con.connect (function (err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log('Connected!');
-})
+
+  // Display items available for sale
+  con.query('SELECT item_id, product_name, price FROM products', function (err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+  // End display items available for sale
+
+});
