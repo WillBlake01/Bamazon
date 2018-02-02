807707665c0d09d72fef21499aefc0f1ba739dbd
William Blake <willblakebooking@gmail.com>
2018-02-02 12:18:46 -0600

    updated ReadMe, just needs .gifs

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # Bamazon
 
 *What does it do?*<br>
-Bamazon is a Command Line Interface (CLI) Node application that provides retail storefront functionality for customers, managers, and supervisors. It's core features include: viewing items and checkout for customers, viewing stock levels and adding new products to database for managers, and viewing Profit & Loss metrics as well as creating new departments for supervisors.
+Bamazon is a Command Line Interface (CLI) Node application that provides retail storefront functionality for customers, managers, and supervisors. It's core features include: viewing items and checkout for customers, viewing stock levels, adding to inventory, and adding new products to database for managers, and viewing Profit & Loss metrics as well as creating new departments to the database for supervisors.
 
 *Why is this useful?*<br>
 Bamazon is a one stop shop retail app run from the command line without the need to navigate to and open several other applications. It contains built in permissions so each role only has access to the commands granted by pay grade or title.
 
 *How can I get started?* <br>
-Just start typing! The commands below initiate the app with separate permissions:<br><br>
-**node bamazonCustomer**<br>
-**node bamazonManager**<br>
-**node bamazonSupervisor**
+Just start typing! Follow the directions below &darr;
+
+## Customer View<br>
+Enter the below in the command line to initiate the customer user interface:<br>
+```
+node bamazonCustomer.js
+```
+
+## Manager View<br>
+Enter the below in the command line to initiate the manager user interface:<br>
+```
+node bamazonManager.js
+```
+
+## Supervisor View<br>
+Enter the below in the command line to initiate the supervisor user interface:<br>
+```
+node bamazonSupervisor.js
+```
 
 *What technologies are used?*<br>
 node.js<br>
 mySQL
 
 *Where can I get help?*<br>
-For help regarding the Node runtime environment please reference the documentation here [https://nodejs.org/en/](). For app specific help please message the address below.
+For help regarding the Node runtime environment please reference the documentation here [https://nodejs.org/en/](https://nodejs.org/en/). For help with mySQL Database Management system please referene the documentation here [https://dev.mysql.com/doc/](https://dev.mysql.com/doc/). For app specific help please message the address below.
 
 *Who maintains and contributes to this project?*<br>
 This project was created by and is maintained by William Blake. Any requests should be forwarded to willblakebooking@gmail.com. Connect with me on [LinkedIn](https://www.linkedin.com/in/william-blake/) or [GitHub](https://github.com/WillBlake01).
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,8 +32,10 @@ function displayItems() {
     // Table parameters
     var header = ['ID', 'Product Name', 'Price', 'Quantity'];
     var table = [];
+    var itemId = [];
     for (var i = 0; i < res.length; i++) {
       table.push(Object.values(res[i]));
+      itemId.push(res[i].item_id);
     }
 
     // Creates table with word-table npm package based on above parameters
@@ -52,7 +54,12 @@ function displayItems() {
           name: 'id',
           message: 'Which item ID would you like to purchase?',
           validate: function (val) {
-            return val > 0 && val <= (res.length);
+            for (var i = 0; i < itemId.length; i++) {
+              var n = itemId[i];
+              if (n == val) {
+                return val > 0 && val == n;
+              }
+            }
           },
         },
         {
@@ -66,19 +73,27 @@ function displayItems() {
     ])
 
     .then(function (answers) {
-        if (res[answers.id - 1].stock_quantity < answers.quantity) {
-          console.log('Insufficient stock! Your order has been cancelled'.error);
-        } else {
-          con.query
-            ('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?',
-          [answers.quantity, answers.id],
-          function (err) {
+        con.query(
+          'SELECT stock_quantity from products WHERE item_id = ?',
+            [answers.id],
+          function (err, res) {
             if (err) throw err;
-            console.log('Your order has been fulfilled!'.info);
-          });
+            if (res[0].stock_quantity < answers.quantity) {
+              console.log('Insufficient stock! Your order has been cancelled'.error);
+              console.log(' ');
+              newPurchase();
+            } else {
+              con.query
+                ('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?',
+              [answers.quantity, answers.id],
+              function (err) {
+                if (err) throw err;
+                console.log('Your order has been fulfilled!'.info);
+              });
 
-          displayTotal(answers);
-        }
+              displayTotal(answers);
+            }
+          });
       });
   });
 }
@@ -89,15 +104,17 @@ function displayItems() {
 
 // Displays total for purchase
 function displayTotal(answers) {
-  con.query('SELECT price FROM products', function (err, res) {
+  con.query('SELECT price FROM products WHERE item_id = ?',
+          [answers.id],
+          function (err, res) {
     if (err) throw err;
-    var totalPrice = answers.quantity * res[answers.id - 1].price;
+    var totalPrice = answers.quantity * res[0].price;
     console.log('Your total is '.info + '$' + totalPrice);
     console.log('Thank you for your purchase!'.magenta);
     console.log('Press Ctrl + C to exit');
     console.log(' ');
     con.query('UPDATE products SET product_sales = product_sales + ? WHERE item_id = ?;',
-      [answers.quantity * res[answers.id - 1].price, answers.id],
+      [answers.quantity * res[0].price, answers.id],
       function (err) {
         if (err) throw err;
       });
